08ff9091ba12ff9db4528c269570426c89395045
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-12-03 15:54:38 +0530

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+const connect = require("./connect");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server Running on Port : ${PORT}`);
+});
